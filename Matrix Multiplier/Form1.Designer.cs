d4c928fc01a257173b770a121f395c20c9db636b
gmgpathum@gmail.com <gmgpathum@gmail.com>
2020-11-23 00:55:46 +0530

    Step 1

---
+++ Matrix Multiplier/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Matrix_Multiplier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpTxt = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.txt8 = new System.Windows.Forms.TextBox();
+            this.txt29 = new System.Windows.Forms.TextBox();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tlpTxt.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(583, 341);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 74.44254F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.55746F));
+            this.tableLayoutPanel1.Controls.Add(this.tlpTxt, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(583, 341);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tlpTxt
+            // 
+            this.tlpTxt.ColumnCount = 3;
+            this.tlpTxt.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.29617F));
+            this.tlpTxt.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 54.70383F));
+            this.tlpTxt.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 139F));
+            this.tlpTxt.Controls.Add(this.txt29, 2, 2);
+            this.tlpTxt.Controls.Add(this.txt8, 1, 2);
+            this.tlpTxt.Controls.Add(this.txt7, 0, 2);
+            this.tlpTxt.Controls.Add(this.txt6, 2, 1);
+            this.tlpTxt.Controls.Add(this.txt5, 1, 1);
+            this.tlpTxt.Controls.Add(this.txt4, 0, 1);
+            this.tlpTxt.Controls.Add(this.txt3, 2, 0);
+            this.tlpTxt.Controls.Add(this.txt2, 1, 0);
+            this.tlpTxt.Controls.Add(this.txt1, 0, 0);
+            this.tlpTxt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpTxt.Location = new System.Drawing.Point(3, 3);
+            this.tlpTxt.Name = "tlpTxt";
+            this.tlpTxt.RowCount = 3;
+            this.tlpTxt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.05882F));
+            this.tlpTxt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.94118F));
+            this.tlpTxt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 113F));
+            this.tlpTxt.Size = new System.Drawing.Size(427, 335);
+            this.tlpTxt.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.btnClear, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.btnCal, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.btnMul, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(436, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.70642F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.29358F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 114F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(144, 335);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // txt1
+            // 
+            this.txt1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt1.BackColor = System.Drawing.SystemColors.Window;
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.ForeColor = System.Drawing.Color.Red;
+            this.txt1.Location = new System.Drawing.Point(25, 33);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(79, 38);
+            this.txt1.TabIndex = 0;
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt2
+            // 
+            this.txt2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt2.BackColor = System.Drawing.SystemColors.Window;
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.ForeColor = System.Drawing.Color.Red;
+            this.txt2.Location = new System.Drawing.Point(169, 33);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(79, 38);
+            this.txt2.TabIndex = 1;
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt3
+            // 
+            this.txt3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt3.BackColor = System.Drawing.SystemColors.Window;
+            this.txt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt3.ForeColor = System.Drawing.Color.Red;
+            this.txt3.Location = new System.Drawing.Point(317, 33);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(79, 38);
+            this.txt3.TabIndex = 2;
+            this.txt3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt4
+            // 
+            this.txt4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt4.BackColor = System.Drawing.SystemColors.Window;
+            this.txt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt4.ForeColor = System.Drawing.Color.Red;
+            this.txt4.Location = new System.Drawing.Point(25, 143);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(79, 38);
+            this.txt4.TabIndex = 3;
+            this.txt4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt5
+            // 
+            this.txt5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt5.BackColor = System.Drawing.SystemColors.Window;
+            this.txt5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt5.ForeColor = System.Drawing.Color.Red;
+            this.txt5.Location = new System.Drawing.Point(169, 143);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(79, 38);
+            this.txt5.TabIndex = 4;
+            this.txt5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt6
+            // 
+            this.txt6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt6.BackColor = System.Drawing.SystemColors.Window;
+            this.txt6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt6.ForeColor = System.Drawing.Color.Red;
+            this.txt6.Location = new System.Drawing.Point(317, 143);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(79, 38);
+            this.txt6.TabIndex = 5;
+            this.txt6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt7
+            // 
+            this.txt7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt7.BackColor = System.Drawing.SystemColors.Window;
+            this.txt7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt7.ForeColor = System.Drawing.Color.Red;
+            this.txt7.Location = new System.Drawing.Point(25, 259);
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(79, 38);
+            this.txt7.TabIndex = 6;
+            this.txt7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt8
+            // 
+            this.txt8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt8.BackColor = System.Drawing.SystemColors.Window;
+            this.txt8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt8.ForeColor = System.Drawing.Color.Red;
+            this.txt8.Location = new System.Drawing.Point(169, 259);
+            this.txt8.Name = "txt8";
+            this.txt8.Size = new System.Drawing.Size(79, 38);
+            this.txt8.TabIndex = 7;
+            this.txt8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt29
+            // 
+            this.txt29.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt29.BackColor = System.Drawing.SystemColors.Window;
+            this.txt29.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt29.ForeColor = System.Drawing.Color.Red;
+            this.txt29.Location = new System.Drawing.Point(317, 259);
+            this.txt29.Name = "txt29";
+            this.txt29.Size = new System.Drawing.Size(79, 38);
+            this.txt29.TabIndex = 8;
+            this.txt29.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnMul
+            // 
+            this.btnMul.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnMul.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnMul.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMul.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnMul.Location = new System.Drawing.Point(11, 33);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(122, 39);
+            this.btnMul.TabIndex = 0;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = false;
+            // 
+            // btnCal
+            // 
+            this.btnCal.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCal.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnCal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnCal.Location = new System.Drawing.Point(11, 142);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(122, 41);
+            this.btnCal.TabIndex = 1;
+            this.btnCal.TabStop = false;
+            this.btnCal.Text = "Calculator";
+            this.btnCal.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnClear.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnClear.Location = new System.Drawing.Point(11, 257);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(121, 41);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.TabStop = false;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 341);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Matrix Multiplier";
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tlpTxt.ResumeLayout(false);
+            this.tlpTxt.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tlpTxt;
+        private System.Windows.Forms.TextBox txt29;
+        private System.Windows.Forms.TextBox txt8;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.Button btnMul;
+    }
+}
+
